680b05d8e31c90d46645eeaa4a0d038205b643f9
pazrachman <paz.rachman@post.runi.ac.il>
2024-12-11 18:17:03 +0200

    HH

--- MyString.java
+++ MyString.java
@@ -13,48 +13,73 @@ public class MyString {
 
     /**
      * Returns the number of times the given character appears in the given string.
-     * Example: countChar("Center",'e') returns 2 and countChar("Center",'c') returns 0. 
+     * Example: countChar("Center",'e') returns 2 and countChar("Center",'c')
+     * returns 0.
      * 
      * @param str - a string
-     * @param c - a character
+     * @param c   - a character
      * @return the number of times c appears in str
      */
     public static int countChar(String str, char ch) {
-        //// Replace the following statement with your code
-        return 0;
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ch) {
+                count++;
+            }
+
+        }
+        return count;
     }
 
-    /** Returns true if str1 is a subset string str2, false otherwise
-     *  Examples:
-     *  subsetOf("sap","space") returns true
-     *  subsetOf("spa","space") returns true
-     *  subsetOf("pass","space") returns false
-     *  subsetOf("c","space") returns true
+    /**
+     * Returns true if str1 is a subset string str2, false otherwise
+     * Examples:
+     * subsetOf("sap","space") returns true
+     * subsetOf("spa","space") returns true
+     * subsetOf("pass","space") returns false
+     * subsetOf("c","space") returns true
      *
      * @param str1 - a string
      * @param str2 - a string
      * @return true is str1 is a subset of str2, false otherwise
      */
     public static boolean subsetOf(String str1, String str2) {
-         //// Replace the following statement with your code
+        for (int i = 0; i < str1.length(); i++) {
+            char ch = str1.charAt(i);
+            if (str2.indexOf(ch) != -1) {
+                return true;
+            }
+
+        }
+
         return false;
     }
 
-    /** Returns a string which is the same as the given string, with a space
+    /**
+     * Returns a string which is the same as the given string, with a space
      * character inserted after each character in the given string, except
-     * for the last character. 
+     * for the last character.
      * Example: spacedString("silent") returns "s i l e n t"
      * 
      * @param str - a string
      * @return a string consisting of the characters of str, separated by spaces.
      */
     public static String spacedString(String str) {
-        //// Replace the following statement with your code
-        return null;
+        String newString = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (i < str.length() - 1) {
+                newString += str.charAt(i) + " "; // return all the chars until the last one with space
+            } else {
+                newString += str.charAt(i); // return the last char without space
+            }
+
+        }
+
+        return newString;
     }
-  
+
     /**
-     * Returns a string of n lowercase letters, selected randomly from 
+     * Returns a string of n lowercase letters, selected randomly from
      * the English alphabet 'a', 'b', 'c', ..., 'z'. Note that the same
      * letter can be selected more than once.
      * 
@@ -64,37 +89,54 @@ public class MyString {
      * @return a randomly generated string, consisting of 'n' lowercase letters
      */
     public static String randomStringOfLetters(int n) {
-        //// Replace the following statement with your code
-        return null;
+        String randomStr = "";
+        for (int i = 0; i < n; i++) {
+            char randomCh = (char) ((int) (Math.random() * 26) + 'a');
+            randomStr += randomCh;
+        }
+
+        return randomStr;
     }
 
     /**
-     * Returns a string consisting of the string str1, minus all the characters in the
+     * Returns a string consisting of the string str1, minus all the characters in
+     * the
      * string str2. Assumes (without checking) that str2 is a subset of str1.
-     * Example: remove("meet","committee") returns "comit" 
+     * Example: remove("meet","committee") returns "comit"
      * 
      * @param str1 - a string
      * @param str2 - a string
      * @return a string consisting of str1 minus all the characters of str2
      */
     public static String remove(String str1, String str2) {
-       //// Replace the following statement with your code
-        return null;
+        if (str2.length() == 0) {
+            return str2;
+        }
+        for (int i = 0; i < str1.length(); i++) {
+            char charAt = str1.charAt(i);
+            if (str2.indexOf(charAt) > -1) {
+                str2 = str2.substring(0, str2.indexOf(charAt)) + str2.substring(str2.indexOf(charAt) + 1);
+
+            }
+        }
+        return str2;
     }
 
     /**
-     * Returns a string consisting of the given string, with the given 
+     * Returns a string consisting of the given string, with the given
      * character inserted randomly somewhere in the string.
-     * For example, insertRandomly("s","cat") can return "scat", or "csat", or "cast", or "cats".  
-     * @param ch - a character
+     * For example, insertRandomly("s","cat") can return "scat", or "csat", or
+     * "cast", or "cats".
+     * 
+     * @param ch  - a character
      * @param str - a string
      * @return a string consisting of str with ch inserted somewhere
      */
     public static String insertRandomly(char ch, String str) {
-         // Generate a random index between 0 and str.length()
-         int randomIndex = (int) (Math.random() * (str.length() + 1));
-         // Insert the character at the random index
-         String result = str.substring(0, randomIndex) + ch + str.substring(randomIndex);
-         return result;
-    }    
+        // Generate a random index between 0 and str.length()
+        int randomIndex = (int) (Math.random() * (str.length() + 1));
+        // Insert the character at the random index
+        String result = str.substring(0, randomIndex) + ch + str.substring(randomIndex);
+        return result;
+    }
 }
